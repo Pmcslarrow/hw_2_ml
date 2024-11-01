cffdbc65f7c34e17130adf4a458415280060a132
Paul McSlarrow <pmcslarrow@icloud.com>
2024-11-01 18:16:50 -0500

    Part II close to complete. About to try adding other features...

--- movie_recommend.py
+++ movie_recommend.py
@@ -23,7 +23,6 @@ Example data:
 user_id 	movie_id	  rating	  title	              genre	  age	  gender	  occupation
 405	      43	        1	        Disclosure (1994)	  Drama	  22	  F	        healthcare
 
-
      user_id  movie_id  rating  title  genre  age  gender  occupation
 0        405        56       3    223      5    0       0           0
 1        405       592       0    310     12    0       0           0
@@ -42,29 +41,20 @@ user_id 	movie_id	  rating	  title	              genre	  age	  gender	  occupati
 | PIVOT THE DATA INTO THIS (BUT WITH ALL USERS) Movie_id on top, user_id's are the rows, and the count are the ratings
 v
 
-movie_id  56  171  580  592  606  904  1470  1478  1582  184
+movie_id  56  171  580  592  606  904  1470  1478  1582  184 
 user_id                                                     
-405       3   0    0    0    2    0    1    0    0    0
+405       3   0    0    0    2    0    1    0    0    0   
+13        n   n    n    n
+655
 
 """
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import collections
+import heapq
 
-def euclidean(a, b):
-  """
-  Parameters
-  ----------
-  The pivoted row of ratings from user1 (a) and user2 (b)
-
-  Returns
-  -------
-  - The euclidean distance between the two users
-  - The movie IDs that user 1 rated but user 2 did not (can use this info for recommendations)
-  - The movie IDs that user 2 rated but user 1 did not (can use this info for recommendations)  
-  """
-
+def cosim(a, b):
   # Drop the NA values from a and b and find where both users have rated the SAME movie
   movie_ids_no_nan_a = a.dropna().index
   movie_ids_no_nan_b = b.dropna().index
@@ -74,11 +64,19 @@ def euclidean(a, b):
   movie_ids_a_ranked_not_b = (movie_ids_no_nan_a.difference(movie_ids_no_nan_b)).tolist()
   movie_ids_b_ranked_not_a = (movie_ids_no_nan_b.difference(movie_ids_no_nan_a)).tolist()
 
-  ratings_a = a[movie_ids_both_rated]
-  ratings_b = b[movie_ids_both_rated]
+  ratings_a = a[movie_ids_both_rated].to_numpy()
+  ratings_b = b[movie_ids_both_rated].to_numpy()
+
+  norm_a = np.linalg.norm(ratings_a)
+  norm_b = np.linalg.norm(ratings_b)
   
-  distance = np.sqrt(np.sum((ratings_a - ratings_b)**2))
-  return distance, movie_ids_a_ranked_not_b, movie_ids_b_ranked_not_a
+  if norm_a == 0 or norm_b == 0:
+    return 0.0
+
+  dot_product = np.dot(ratings_a, ratings_b)
+  print("DOT: ", dot_product)
+  similarity = (dot_product / (norm_a * norm_b))
+  return similarity
 
 def calculate_similarities(data):
   """
@@ -88,10 +86,9 @@ def calculate_similarities(data):
 
   Returns
   -------
-  Returns a dictionary that represents the similarity between each user... It looks spooky, but it is as simple as this:
-  { user_1: [(distance, user_2, movie ids user 1 rated but user 2 did not, movie ids user 2 rated but user 1 did not)]}
+  NOTE COME BACK
   """
-  similarities = collections.defaultdict(list)
+  similarities = collections.defaultdict(lambda: collections.defaultdict(float))
   visited = set()
   users = data.index.tolist() 
   for i in range(len(users)):
@@ -100,10 +97,56 @@ def calculate_similarities(data):
       user_2 = users[j]
       if user_1 != user_2 and tuple(sorted((user_1, user_2))) not in visited:
         visited.add(tuple(sorted((user_1, user_2))))
-        distance, movie_ids_user1_not_user2, movie_ids_user2_not_user1 = euclidean(data.loc[user_1], data.loc[user_2])
-        similarities[user_1].append((distance, user_2, movie_ids_user1_not_user2, movie_ids_user2_not_user1))
-        similarities[user_2].append((distance, user_1, movie_ids_user1_not_user2, movie_ids_user2_not_user1))
+        distance = cosim(data.loc[user_1], data.loc[user_2])
+        similarities[user_1][user_2] = distance
+        similarities[user_2][user_1] = distance
+        # similarities[user_1].append((distance, user_2))
+        # similarities[user_2].append((distance, user_1))
   return similarities
+
+def impute_rating(pivot_table, similarity_dict):
+  rows, cols = pivot_table.shape
+  recommendations = collections.defaultdict(list)
+
+  for movie_id in pivot_table.columns:
+    ratings_per_movie_id = pivot_table[movie_id]
+    nan_count = ratings_per_movie_id.isna().sum()
+    difference = rows - nan_count
+    if difference < 2:
+      continue
+
+    builder = {}
+    for user_id, rating in ratings_per_movie_id.items():
+      builder[user_id] = rating
+    
+    for user_id, rating in builder.items():
+      if np.isnan(rating):
+        continue
+
+      imputed_rating_num = 0
+      imputed_rating_denom = 0
+      
+      for other_user_id, other_rating in builder.items():
+        if other_user_id == user_id or np.isnan(other_rating):
+          continue
+        imputed_rating_num += (other_rating * similarity_dict[user_id][other_user_id])
+        imputed_rating_denom += similarity_dict[user_id][other_user_id]
+
+      recommendation = imputed_rating_num / imputed_rating_denom
+      recommendation = min(max(recommendation, 0), 5)
+      heapq.heappush(recommendations[user_id], (recommendation, movie_id))
+      
+      if len(recommendations[user_id]) > 5:
+        heapq.heappop(recommendations[user_id])
+  return recommendations
+
+      
+    
+      
+      
+  
+
+
 
 def read_file(prefix="train"):
   """
@@ -122,11 +165,6 @@ def read_file(prefix="train"):
   for letter in postfix:
     filename = f"{prefix}_{letter}.txt"
     data = pd.read_csv(filename, sep='\t')
-    label_encoder = LabelEncoder()
-
-    for column_name in data.columns:
-      if column_name not in ['user_id', 'movie_id']:
-        data[column_name] = label_encoder.fit_transform(data[column_name])
     training_datasets.append(data)
   return training_datasets
 
@@ -141,12 +179,23 @@ if __name__ == '__main__':
     test_data = pd.concat(test_datasets, ignore_index=True)
     valid_data = pd.concat(validation_datasets, ignore_index=True)
 
+    genre_encoder = LabelEncoder()
+    gender_encoder = LabelEncoder()
+    occupation_encoder = LabelEncoder()
+
+    training_data['genre'] = genre_encoder.fit_transform(training_data['genre'])
+    training_data['gender'] = gender_encoder.fit_transform(training_data['gender'])
+    training_data['occupation'] = occupation_encoder.fit_transform(training_data['occupation'])
+
     # Creating User-based collaborative filtering
     train_pivot = training_data.pivot(index='user_id', columns='movie_id', values='rating')
     training_user_similarities = calculate_similarities(train_pivot)
+    
 
-    for user_id, distance_tuple in training_user_similarities.items():
-      print(f"User ID: {user_id}")
-      for distance, other_user, _, _ in distance_tuple:
-        print(f"- Distance to user ID {other_user} == {distance}")
-      print()
+    impute_rating(train_pivot, training_user_similarities)
+
+    # for user_id, distance_tuple in training_user_similarities.items():
+    #   print(f"User ID: {user_id}")
+    #   for distance, other_user, _, _ in distance_tuple:
+    #     print(f"- Distance to user ID {other_user} == {distance}")
+    #   print()
